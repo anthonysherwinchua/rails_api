e925ab64bfd41972893a75ac685a5c5880d4a507
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-05 09:00:14 +0800

    prepare V1 API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+    end
+
+    # catch-all other vN and redirect it to the latest version
+    match 'v:api/*path', :to => redirect("/api/v1/%{path}"), via: [:get, :post, :put, :patch, :delete]
+  end
 end
